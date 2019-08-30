264875d9d805977eea1e62ea0ffe8c6ff875b75a
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2019-08-30 04:25:26 +0100

    Created the xml layout and set button onClickListeners for respective buttons

--- app/src/main/java/com/project/gofoodie/MenuActivity.java
+++ app/src/main/java/com/project/gofoodie/MenuActivity.java
@@ -2,13 +2,70 @@ package com.project.gofoodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        button = (Button) findViewById(R.id.swallow);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openswallow();
+            }
+
+        });
+
+        button = (Button) findViewById(R.id.soup_stew);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opensoup();
+            }
+
+        });
+
+        button = (Button) findViewById(R.id.meat);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmeat();
+            }
+
+        });
+
+        button = (Button) findViewById(R.id.drinks);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendrinks();
+            }
+
+        });
+    }
+    public void openswallow(){
+        Intent intent = new Intent(this, swallow.class);
+        startActivity(intent);
+    }
+    public void opensoup(){
+        Intent intent = new Intent(this, soup.class);
+        startActivity(intent);
+    }
+    public void openmeat(){
+        Intent intent = new Intent(this, meat.class);
+        startActivity(intent);
+    }
+    public void opendrinks(){
+        Intent intent = new Intent(this, drinks.class);
+        startActivity(intent);
     }
 }
