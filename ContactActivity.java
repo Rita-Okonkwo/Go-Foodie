cbf71692bf705c67a28bb23a91e1832d15d30e2f
Jolugba <52603037+Jolugba@users.noreply.github.com>
2019-08-31 20:03:22 +0100

    Add files via upload
    updated repo

---
+++ ContactActivity.java
@@ -0,0 +1,18 @@
+package com.project.gofoodie;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        getActionBar().setTitle("    Contact Us");
+    }
+}
